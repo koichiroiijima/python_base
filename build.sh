2c8babd56bdf634748ea5bf8d351ce52e79da5cd
koichiroiijima <koichiro.iijima.1979@gmail.com>
2019-07-27 21:44:17 -0700

    Support Multiple versions builds.

--- build.sh
+++ build.sh
@@ -2,15 +2,17 @@
 set -ex
 cd "$(dirname "$0")"
 
-PYTHON_VERSION=${PYTHON_VERSION:-3.7.4}
+PYTHON_VERSION=${1:-3.7.4}
 ALPINE_VERSION="alpine3.10"
 VERSION=${PYTHON_VERSION}-${ALPINE_VERSION}-0.0.2-20190727
 
+
+echo "***** ${PYTHON_VERSION} *****"
 docker build . --squash -t python_base:${VERSION} --build-arg IMAGE_VERSION=${VERSION} --build-arg IMAGE_NAME="python_base" --build-arg PYTHON_VERSION=${PYTHON_VERSION}
 docker tag python_base:${VERSION} python_base:latest
 
 docker login
 docker tag python_base:${VERSION} koichiroiijima/python_base:${VERSION}
-docker tag python_base:${VERSION} koichiroiijima/python_base:latest
+#docker tag python_base:${VERSION} koichiroiijima/python_base:latest
 docker push koichiroiijima/python_base:${VERSION}
-docker push koichiroiijima/python_base:latest
+#docker push koichiroiijima/python_base:latest
